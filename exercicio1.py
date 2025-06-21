750ffa417034b7c9c98371844ce403a6d182ebfc
Mendes_Devv <renanmendescontato3@gmail.com>
2025-06-20 21:45:50 -0300

    Add files via upload

---
+++ exercicio1.py
@@ -0,0 +1,18 @@
+nome = 'Renan' 
+sobrenome = 'Mendes'
+idade = 20
+ano_nascimento = 2024 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.90
+
+
+
+
+
+print('Nome' , nome)
+print('Sobrenome:', sobrenome)
+print('Idade', idade)
+print('Ano de Nascimento:', ano_nascimento)   
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
+
